1f12d3e06f9b41e485957d4d83ed6ca406c8fcaa
JUAREZ.DEV <147560945+LucasJuarezDev@users.noreply.github.com>
2024-05-26 19:16:39 -0300

    Update CostoAgregado.h

--- CostoAgregado.h
+++ CostoAgregado.h
@@ -1,6 +1,21 @@
 #ifndef COSTOAGREGADO_H_INCLUDED
 #define COSTOAGREGADO_H_INCLUDED
 
-
+class CostoAgregado
+{
+private:
+    int CodigoPrenda;
+    float CostoPackaging;
+    float CostoEtiqueta;
+public:
+    CostoAgregado(int codigo_prenda, float costo_packaging, float costo_etiqueta);
+    int getCodigo();
+    float getPackaging();
+    float getEtiqueta();
+    void setCodigo(int codigo_prenda);
+    void setPackaging(float costo_packaging);
+    void setEtiqueta(float costo_etiqueta);
+};
 
 #endif // COSTOAGREGADO_H_INCLUDED
+

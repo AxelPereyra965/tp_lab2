6c5f42a36bc9b44befd72f73a2d2d4bcf14c8801
JUAREZ.DEV <147560945+LucasJuarezDev@users.noreply.github.com>
2024-05-26 19:15:16 -0300

    MenuPrincipal

--- main.cpp
+++ main.cpp
@@ -1,9 +1,51 @@
 #include <iostream>
+#include "ManagerCostoAgregado.h"
+#include "ManagerPrenda.h"
+#include "ManagerTaller.h"
+#include "ManagerVenta.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int opcion;
+    while (true){
+        system("cls");
+        cout << "MENU PRINCIPAL" << endl;
+        cout << "----------------" << endl;
+        cout << "1) PRENDA" << endl;
+        cout << "2) INVENTARIO" << endl;
+        cout << "3) ESTADISTICAS" << endl;
+        cout << "---------------------------" << endl;
+        cout << "0) SALIR" << endl;
+
+        cin >> opcion;
+
+        switch(opcion){
+            case 1:
+                {
+                    
+                }
+                break;
+            case 2:
+                {
+
+                }
+                break;
+            case 3:
+                {
+
+                }
+                break;
+            case 0:
+                {
+                    return 0;
+                }
+                break;
+            default:
+                cout << "OPCION INCORRECTA" << endl;
+            break;
+        }
+
+    }
 }

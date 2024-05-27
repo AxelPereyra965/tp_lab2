e4e1cc8db7d3df224e703d5121fbb9a572f884c3
JUAREZ.DEV <147560945+LucasJuarezDev@users.noreply.github.com>
2024-05-27 20:46:27 -0300

    Update Prenda.cpp

--- Prenda.cpp
+++ Prenda.cpp
@@ -1,1 +1,51 @@
 #include "Prenda.h"
+
+int Prenda :: getCodigo()
+{
+  return CodigoPrenda;
+}
+
+string Prenda :: getNombrePrenda()
+{
+  return NombrePrenda;
+}
+
+string Prenda :: getModelo()
+{
+  return Modelo;
+}
+
+string Prenda :: getTalle()
+{
+  return Talle;
+}
+
+string Prenda :: getColor()
+{
+  return Color;
+}
+
+string Prenda :: getCampania()
+{
+  return Campania;
+}
+
+int Prenda :: getCantidad()
+{
+  return Cantidad;
+}
+
+float Prenda ::  getPorcentajeDeGanancia()
+{
+  return PorcentajeDeGanancia;
+}
+
+bool Prenda :: getEstadoDePrenda()
+{
+  return EstadoDePrenda;
+}
+
+void Prenda :: setCodigoDePrenda(int codigo_prenda)
+{
+  CodigoPrenda = codigo_prenda;
+}

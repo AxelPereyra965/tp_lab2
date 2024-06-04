5ca573026b06daac3596ecdec1ddc585b67d6f08
JUAREZ.DEV <147560945+LucasJuarezDev@users.noreply.github.com>
2024-06-03 21:53:10 -0300

    ManagerPrenda y sus submenus

--- PrendaManager.h
+++ PrendaManager.h
@@ -1,6 +1,21 @@
 #ifndef PRENDAMANAGER_H_INCLUDED
 #define PRENDAMANAGER_H_INCLUDED
+#include "Prenda.h"
+#include "PrendaArchivo.h"
 
+class PrendaManager
+{
+private:
+    PrendaArchivo Arch;
+    void ModificacionDePrenda(Prenda &obj);
+    Prenda CargarPrenda();
+    void MostrarPrenda(Prenda obj);
+public:
+    void SubmenuCargarPrenda();
+    void SubmenuInventario();
+    void SubmenuModificarPrenda();
+    void SubmenuEliminarPrenda();
 
+};
 
 #endif // PRENDAMANAGER_H_INCLUDED

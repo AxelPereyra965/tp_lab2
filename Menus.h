9eea9f56e2d97febad66e8ed0a5475991e17aa6a
JUAREZ.DEV <147560945+LucasJuarezDev@users.noreply.github.com>
2024-06-23 23:29:00 -0300

    PRECIOTOTAL EN VENTA + MEJORAS DE FRONT

--- Menus.h
+++ Menus.h
@@ -1,9 +1,10 @@
 #ifndef MENUS_H_INCLUDED
 #define MENUS_H_INCLUDED
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 #include "PrendaManager.h"
 #include "VentaManager.h"
-#include <iostream>
-using namespace std;
 
 //      MENU DE PRENDA
 
@@ -16,14 +17,20 @@ void Menu1()
      int opcion;
     while (true){
         system("cls");
-        cout << "PRENDAS" << endl;
-        cout << "----------------" << endl;
-        cout << "1) CARGAR PRENDA" << endl;
-        cout << "2) MODIFICAR PRENDA" << endl;
-        cout << "3) ELIMINAR PRENDA" << endl;
-        cout << "4) CARGAR VENTA" << endl;
+        cout << "         PRENDAS" << endl;
         cout << "---------------------------" << endl;
-        cout << "0) MENU ANTERIOR" << endl;
+        cout << endl;
+        cout << "1)    CARGAR PRENDA" << endl;
+        cout << endl;
+        cout << "2)   MODIFICAR PRENDA" << endl;
+        cout << endl;
+        cout << "3)   ELIMINAR PRENDA" << endl;
+        cout << endl;
+        cout << "4)    CARGAR VENTA" << endl;
+        cout << endl;
+        cout << "---------------------------" << endl;
+        cout << endl;
+        cout << "0)   MENU ANTERIOR" << endl;
 
         cin >> opcion;
 
@@ -56,8 +63,13 @@ void Menu1()
                 }
                 break;
             default:
+                if(cin.fail())
+                {
+                cin.clear();
+                cin.ignore();
                 cout << "OPCION INCORRECTA" << endl;
                 system("pause");
+                }
             break;
         }
 
@@ -71,12 +83,16 @@ void Menu2()
     int opcion;
     while (true){
         system("cls");
-        cout << "INVENTARIO" << endl;
-        cout << "----------------" << endl;
-        cout << "1) VER INVENTARIO" << endl;
-        cout << "2) DETALLES DE PRENDA" << endl;
+        cout << "       INVENTARIO" << endl;
+        cout << "--------------------------" << endl;
+        cout << endl;
+        cout << "1)    VER INVENTARIO" << endl;
+        cout << endl;
+        cout << "2)   DETALLES DE PRENDA" << endl;
+        cout << endl;
         cout << "---------------------------" << endl;
-        cout << "0) MENU ANTERIOR" << endl;
+        cout << endl;
+        cout << "0)    MENU ANTERIOR" << endl;
 
         cin >> opcion;
 
@@ -99,8 +115,13 @@ void Menu2()
                 }
                 break;
             default:
+                if(cin.fail())
+                {
+                cin.clear();
+                cin.ignore();
                 cout << "OPCION INCORRECTA" << endl;
                 system("pause");
+                }
             break;
         }
 
@@ -114,16 +135,20 @@ void Menu3()
     int opcion;
     while (true){
         system("cls");
-        cout << "ESTADISTICA" << endl;
-        cout << "----------------" << endl;
-        cout << "1) ESTADISTICAS DE PRODUCTOS" << endl;
-        cout << "2) HISTORIAL DE VENTA" << endl;
-        cout << "3) PORCENTAJE DE GANANCIAS POR MES" << endl;
+        cout << "      ESTADISTICA" << endl;
+        cout << "-------------------------" << endl;
+        cout << endl;
+        cout << "1)  ESTADISTICAS DE PRODUCTOS" << endl;
+        cout << endl;
+        cout << "2)   HISTORIAL DE VENTA" << endl;
+        cout << endl;
+        cout << "3)  PORCENTAJE DE GANANCIAS POR MES" << endl;
+        cout << endl;
         cout << "---------------------------" << endl;
-        cout << "0) MENU ANTERIOR" << endl;
+        cout << endl;
+        cout << "0)    MENU ANTERIOR" << endl;
 
         cin >> opcion;
-
         switch(opcion){
             case 1:
                 {
@@ -148,8 +173,13 @@ void Menu3()
                 }
                 break;
             default:
+                if(cin.fail())
+                {
+                cin.clear();
+                cin.ignore();
                 cout << "OPCION INCORRECTA" << endl;
                 system("pause");
+                }
             break;
         }
 
